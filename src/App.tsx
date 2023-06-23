1b33561de26e088ff73a4f0aaabc4f1995bed25e
Rohit <rohitmalik987789@gmail.com>
2023-06-23 22:23:04 +0530

    linear gradient for background

--- src/App.tsx
+++ src/App.tsx
@@ -2,56 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import { Background } from './features/background/Background';
 import './App.css';
+import { useAppSelector, useAppDispatch } from './app/hooks';
+import {
+    addNewBackground,
+    selectBackground,
+} from './features/background/backgroundSlice'
 
 function App() {
+    let background = useAppSelector(selectBackground);
+    const dispatch = useAppDispatch();
+    const changeBackground = () => {
+        if(background.type === 'color') {
+            dispatch(addNewBackground({type: 'gradient', value: '#6D92CA-#A22AEE'}));
+        }
+        if(background.type === 'gradient') {
+            dispatch(addNewBackground({type: 'color', value: '#ccffcc'}));
+        }
+    }
     return (
         <div className="App">
             <Background />
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            <div>asdfasf</div>
-            asd
+            <button
+            onClick={changeBackground}
+            >change</button>
         </div>
     );
 }
--- src/features/background/Background.tsx
+++ src/features/background/Background.tsx
@@ -11,11 +11,23 @@ const Background: React.FC = () => {
         if (background.type === "color") {
             changeBackgroundColour(background.value);
         }
+        if (background.type === "gradient") {
+            changeBackgroundGradient(background.value.split('-'));
+        }
+
     }, [background]);
     console.log(background);
     const changeBackgroundColour = (hexValue: string) => {
-        document.querySelector('.BACKGROUND')?.setAttribute('style', `background-color: ${hexValue};`)
+        const backgroundEle = document.querySelector('.BACKGROUND');
+        backgroundEle?.removeAttribute('style');
+        backgroundEle?.setAttribute('style', `background-color: ${hexValue};`)
         console.log('changeBackgroundColour');
+    }
+    const changeBackgroundGradient = (gradientValue: string[]) => {
+        const backgroundEle = document.querySelector('.BACKGROUND');
+        backgroundEle?.removeAttribute('style');
+        backgroundEle?.setAttribute('style', `background-image: linear-gradient(${gradientValue[0]}, ${gradientValue[1]});`)
+        console.log('changeBackgroundGradient');
     }
     return (
         <div className='BACKGROUND fixed top-0 left-0 w-full h-full -z-50'>
