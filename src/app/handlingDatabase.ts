05f874436770c9b85f1a7c980a2b8e505510f4ff
Rohit <rohitmalik987789@gmail.com>
2023-07-08 00:54:45 +0530

    added some database funciton:- two new store for 144pimage and video, getter and setter for the stores, and delte functionality for all store

--- src/app/handlingDatabase.ts
+++ src/app/handlingDatabase.ts
@@ -1,5 +1,97 @@
 import React from "react";
 import { themeSliceProps } from "../features/theme/themeSlice";
+
+export const changeImageQualityTo144p = (
+    imageData: ArrayBuffer
+): Promise<ArrayBuffer | null> => {
+    // returns a promise that resolves to the resized image data while maintaining aspect ratio
+    return new Promise<ArrayBuffer | null>((resolve, reject) => {
+        const img = new Image();
+        img.src = URL.createObjectURL(new Blob([imageData]));
+        img.onload = () => {
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
+            if (ctx === null) {
+                resolve(null);
+                return;
+            }
+            const width = img.width;
+            const height = img.height;
+            const aspectRatio = width / height;
+            const newWidth = 256 * aspectRatio;
+            const newHeight = 256;
+            canvas.width = newWidth;
+            canvas.height = newHeight;
+            ctx.drawImage(img, 0, 0, newWidth, newHeight);
+            canvas.toBlob(
+                (blob) => {
+                    if (blob === null) {
+                        resolve(null);
+                        return;
+                    }
+                    const reader = new FileReader();
+                    reader.readAsArrayBuffer(blob);
+                    reader.onloadend = () => {
+                        const data = reader.result;
+                        if (data === null || typeof data === "string") {
+                            resolve(null);
+                            return;
+                        }
+                        resolve(data);
+                    };
+                },
+                "image/jpeg",
+                0.5
+            );
+        };
+    });
+};
+
+export const changeVideoQualityTo144p = ( videoData: ArrayBuffer): Promise<ArrayBuffer | null> => {
+    // returns video(scaled to 144p) with same aspect ratio
+    return new Promise<ArrayBuffer | null>((resolve, reject) => {
+        const video = document.createElement("video");
+        video.src = URL.createObjectURL(new Blob([videoData]));
+        video.onloadedmetadata = () => {
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
+            if (ctx === null) {
+                resolve(null);
+                return;
+            }
+            const width = video.videoWidth;
+            const height = video.videoHeight;
+            const aspectRatio = width / height;
+            const newWidth = 256 * aspectRatio;
+            const newHeight = 256;
+            canvas.width = newWidth;
+            canvas.height = newHeight;
+            ctx.drawImage(video, 0, 0, newWidth, newHeight);
+            canvas.toBlob(
+                (blob) => {
+                    if (blob === null) {
+                        resolve(null);
+                        return;
+                    }
+                    const reader = new FileReader();
+                    reader.readAsArrayBuffer(blob);
+                    reader.onloadend = () => {
+                        const data = reader.result;
+                        if (data === null || typeof data === "string") {
+                            resolve(null);
+                            return;
+                        }
+                        resolve(data);
+                    };
+                },
+                "video/mp4",
+                0.5
+            );
+        };
+    });
+};
+
+
 export const createDatabase = () => {
     // database to store images
     let req = indexedDB.open("backgroundData", 1);
@@ -28,10 +120,198 @@ export const createDatabase = () => {
             "data",
             { unique: true }
         );
+        db.createObjectStore("lowResImages", { keyPath: "id" }).createIndex(
+            "data",
+            "data",
+            { unique: true }
+        );
+        db.createObjectStore("lowResVideos", { keyPath: "id" }).createIndex(
+            "data",
+            "data",
+            { unique: true }
+        );
     };
     req.onsuccess = () => {
         console.log("Database opened");
     };
+};
+
+export const deleteImageFromDatabase = (id: string) => {
+    // delete image from database with given id (original image and 144p image)
+    let req = indexedDB.open("backgroundData", 1);
+    req.onsuccess = () => {
+        let db = req.result;
+        let tx = db.transaction("images", "readwrite");
+        let store = tx.objectStore("images");
+        let reqDelete = store.delete(id);
+        reqDelete.onsuccess = () => {
+            console.log("Image deleted from DB");
+        };
+        reqDelete.onerror = () => {
+            console.log("Error deleting image from DB");
+        };
+        let tx2 = db.transaction("lowResImages", "readwrite");
+        let store2 = tx2.objectStore("lowResImages");
+        let reqDelete2 = store2.delete(id);
+        reqDelete2.onsuccess = () => {
+            console.log("Image deleted from DB");
+        };
+        reqDelete2.onerror = () => {
+            console.log("Error deleting image from DB");
+        };
+    };
+};
+
+export const deleteVideoFromDatabase = (id: string) => {
+    // delete video from database with given id (original video and 144p video)
+    let req = indexedDB.open("backgroundData", 1);
+    req.onsuccess = () => {
+        let db = req.result;
+        let tx = db.transaction("videos", "readwrite");
+        let store = tx.objectStore("videos");
+        let reqDelete = store.delete(id);
+        reqDelete.onsuccess = () => {
+            console.log("Video deleted from DB");
+        };
+        reqDelete.onerror = () => {
+            console.log("Error deleting video from DB");
+        };
+        let tx2 = db.transaction("lowResVideos", "readwrite");
+        let store2 = tx2.objectStore("lowResVideos");
+        let reqDelete2 = store2.delete(id);
+        reqDelete2.onsuccess = () => {
+            console.log("Video deleted from DB");
+        };
+        reqDelete2.onerror = () => {
+            console.log("Error deleting video from DB");
+        };
+    };
+};
+
+export const deleteColorFromDatabase = (id: string) => {
+    // delete color from database with given id
+    let req = indexedDB.open("backgroundData", 1);
+    req.onsuccess = () => {
+        let db = req.result;
+        let tx = db.transaction("colors", "readwrite");
+        let store = tx.objectStore("colors");
+        let reqDelete = store.delete(id);
+        reqDelete.onsuccess = () => {
+            console.log("Color deleted from DB");
+        };
+        reqDelete.onerror = () => {
+            console.log("Error deleting color from DB");
+        };
+    };
+};
+
+export const deleteGradientFromDatabase = (id: string) => {
+    // delete gradient from database with given id
+    let req = indexedDB.open("backgroundData", 1);
+    req.onsuccess = () => {
+        let db = req.result;
+        let tx = db.transaction("gradients", "readwrite");
+        let store = tx.objectStore("gradients");
+        let reqDelete = store.delete(id);
+        reqDelete.onsuccess = () => {
+            console.log("Gradient deleted from DB");
+        };
+        reqDelete.onerror = () => {
+            console.log("Error deleting gradient from DB");
+        };
+    };
+};
+
+
+export const addLocalVideo144p = (
+    id: string,
+    videoData: ArrayBuffer | null
+) => {
+    // add video to database
+    let req = indexedDB.open("backgroundData", 1);
+    req.onsuccess = () => {
+        let db = req.result;
+        let tx = db.transaction("lowResVideos", "readwrite");
+        let store = tx.objectStore("lowResVideos");
+        let reqAdd = store.put({
+            id: id,
+            data: videoData,
+        });
+        reqAdd.onsuccess = () => {
+            console.log("Video added to DB");
+        };
+        reqAdd.onerror = () => {
+            console.log("Error adding video to DB");
+        };
+    };
+};
+
+export const getLocalVideo144p = (id: string) => {
+    return new Promise<ArrayBuffer | null>((resolve, reject) => {
+        let req = indexedDB.open("backgroundData", 1);
+        req.onsuccess = () => {
+            let db = req.result;
+            let tx = db.transaction("lowResVideos", "readonly");
+            let store = tx.objectStore("lowResVideos");
+            let reqGet = store.get(id);
+            reqGet.onsuccess = () => {
+                let data = reqGet.result;
+                if (data === undefined) {
+                    resolve(null);
+                    return;
+                }
+                resolve(data.data);
+            };
+            reqGet.onerror = () => {
+                resolve(null);
+            };
+        };
+        req.onerror = () => {
+            resolve(null);
+        };
+    });
+};
+
+export const addLocalImage144p = (
+    id: string,
+    imageData: ArrayBuffer | null
+) => {
+    // add image to database
+    let req = indexedDB.open("backgroundData", 1);
+    req.onsuccess = () => {
+        let db = req.result;
+        let tx = db.transaction("lowResImages", "readwrite");
+        let store = tx.objectStore("lowResImages");
+        let reqAdd = store.put({
+            id: id,
+            data: imageData,
+        });
+        reqAdd.onsuccess = () => {
+            console.log("Image added to DB");
+        };
+        reqAdd.onerror = () => {
+            console.log("Error adding image to DB");
+        };
+    };
+};
+
+export const getLocalImage144p = (id: string) => {
+    return new Promise<ArrayBuffer | null>((resolve, reject) => {
+        let req = indexedDB.open("backgroundData", 1);
+        req.onsuccess = () => {
+            let db = req.result;
+            let tx = db.transaction("lowResImages", "readonly");
+            let store = tx.objectStore("lowResImages");
+            let reqGet = store.get(id);
+            reqGet.onsuccess = () => {
+                if (reqGet.result === undefined) {
+                    resolve(null);
+                } else {
+                    resolve(reqGet.result.data);
+                }
+            };
+        };
+    });
 };
 
 export const addLocalImage = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -46,11 +326,18 @@ export const addLocalImage = (event: React.ChangeEvent<HTMLInputElement>) => {
     for (let i = 0; i < files.length; i++) {
         const file = files[i];
         const reader = new FileReader();
-        reader.onload = function (event) {
-            const binaryData = event.target?.result;
+        reader.onload = async function (event) {
+            let binaryData = event.target?.result;
+            if (typeof binaryData === "string" || binaryData === undefined) {
+                return;
+            }
             if (binaryData === null) {
                 return;
             }
+            // resize image to 144p
+            let bd = await changeImageQualityTo144p(binaryData);
+            addLocalImage144p(file.name, bd);
+            console.log(bd);
             let req = indexedDB.open("backgroundData", 1);
             req.onsuccess = () => {
                 let db = req.result;
@@ -218,8 +505,8 @@ export const addSolidColor = (color: string) => {
     let req = indexedDB.open("backgroundData", 1);
     req.onsuccess = () => {
         let db = req.result;
-        let tx = db.transaction("solidColors", "readwrite");
-        let store = tx.objectStore("solidColors");
+        let tx = db.transaction("colors", "readwrite");
+        let store = tx.objectStore("colors");
         // check if color already exists
         let reqGet = store.get(color);
         reqGet.onsuccess = () => {
@@ -228,13 +515,14 @@ export const addSolidColor = (color: string) => {
                 // add color to database
                 let reqAdd = store.put({
                     id: color,
-                    data: "",
+                    data: color,
                 });
                 reqAdd.onsuccess = () => {
                     console.log("Color added to DB");
                 };
-                reqAdd.onerror = () => {
+                reqAdd.onerror = (e) => {
                     console.log("Error adding color to DB");
+                    console.log(e);
                 };
             } else {
                 console.log("Color already exists");
@@ -248,8 +536,8 @@ export const getSolidColorList = () => {
         let req = indexedDB.open("backgroundData", 1);
         req.onsuccess = () => {
             let db = req.result;
-            let tx = db.transaction("solidColors", "readonly");
-            let store = tx.objectStore("solidColors");
+            let tx = db.transaction("colors", "readonly");
+            let store = tx.objectStore("colors");
             let reqGet = store.getAllKeys();
             reqGet.onsuccess = () => {
                 resolve(reqGet.result);
@@ -278,7 +566,7 @@ export const addGradient = (gradient: string) => {
                 // add gradient to database
                 let reqAdd = store.put({
                     id: gradient,
-                    data: "",
+                    data: gradient,
                 });
                 reqAdd.onsuccess = () => {
                     console.log("Gradient added to DB");
