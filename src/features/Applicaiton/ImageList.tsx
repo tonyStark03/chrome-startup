d3c51af042bdf71ccb2c0adf314e6ec676a6d5b1
Rohit <rohitmalik987789@gmail.com>
2023-07-09 13:34:42 +0530

    add and delete for live and static background

--- src/features/Applicaiton/ImageList.tsx
+++ src/features/Applicaiton/ImageList.tsx
@@ -1,8 +1,14 @@
 import React, { useEffect } from 'react'
-import { getLocalImageList } from '../../app/handlingDatabase'
+import { addLocalImage, getLocalImageList } from '../../app/handlingDatabase'
 import Image from './Image'
-const ImageList:React.FC = () => {
+import { HiPlus } from 'react-icons/hi'
+import { motion } from 'framer-motion'
+const ImageList: React.FC = () => {
     const [imageList, setImageList] = React.useState<IDBValidKey[]>([])
+    const [newimageList, setNewImageList] = React.useState<IDBValidKey[]>([])
+    const deleteItem = (id: IDBValidKey) => {
+        setImageList(imageList.filter((item) => item !== id))
+    }
     useEffect(() => {
         const t = async () => {
             const list = await getLocalImageList()
@@ -16,6 +22,17 @@ const ImageList:React.FC = () => {
         console.log(imageList)
 
     }, [imageList])
+    function hashCode(str: string) {
+        let hash = 0;
+        for (let i = 0; i < str.length; i++) {
+            const char = str.charCodeAt(i);
+            hash = (hash << 5) - hash + char;
+            hash |= 0; // Convert to 32-bit integer
+        }
+        return hash;
+    }
+
+    const inputEl = React.useRef<HTMLInputElement>(null);
     return (
         <div className='w-full flex flex-row overflow-x-scroll  h-full no-scrollbar 
             '
@@ -25,15 +42,107 @@ const ImageList:React.FC = () => {
                 scrollBehavior: 'smooth',
                 overflow: 'hidden',
             }}
-        >
-            {imageList.map((imageId,index) => {
-                return (
-                    <Image id={imageId}
-                    key={index}
-                    />
+        >{ }
+            {
+                imageList.length + newimageList.length < 10 &&
+                (
+                    <>
+                        <input
+                            ref={inputEl}
+                            type="file"
+                            accept="image/*"
+                            style={{
+                                display: 'none'
+                            }}
+                            onChange={async (e) => {
+                                addLocalImage(e)
+                                let f = true
+                                while (f) {
+                                    let LIST = await getLocalImageList()
+                                    if (LIST.length > imageList.length) {
+                                        let diff = LIST.filter(element => !imageList.includes(element))
+                                        let newdiff = diff.filter(element => !newimageList.includes(element))
+                                        setImageList([...newdiff, ...imageList])
+                                        f = false
+                                    }
+                                }
+                            }}
+                        ></input>
+                        <motion.div
+                            onClick={() => {
+                                inputEl.current?.click()
+                            }}
+                            initial={{
+                                scale: 0,
+                            }}
+                            animate={{
+                                scale: 1,
+                                transition: {
+                                    duration: 0.2
+                                }
+                            }}
+                            whileHover={{
+                                scale: 21 / 20,
+                                x: "0.5rem",
+                                y: (21 / 20) / 2 * 0.5 + "rem",
+                            }}
+                            transition={{
+                                duration: 0.5,
+                                ease: 'easeInOut',
+                            }}
+                            style={{
+                                background: 'rgba(255,255,255,0.1)',
+                                height: "14rem",
+                                width: '20rem',
+                                display: 'inline-block',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                flex: "0 0 auto",
+                                margin: '0 0.5rem',
+                            }}
+                            className='w-full h-full'
+                        >
+                            <motion.div
+                                initial={{
+                                    scale: 0,
+                                }}
+                                animate={{
+                                    scale: 1,
+                                    rotate: 90,
+                                    transition: {
+                                        duration: 0.2
+                                    }
+                                }}
+                                whileHover={{
+                                    scale: 21 / 20,
+                                    rotate: 180,
+                                    transition: {
+                                        duration: 0.2
+                                    }
+                                }}
+                                className='w-full h-full flex justify-center items-center'
+                            >
+                                <HiPlus className='text-white text-9xl'
+                                />
+                            </motion.div>
+
+                        </motion.div>
+                    </>
+
                 )
             }
-            )}
+            {
+                imageList.map((imageId, index) => {
+                    return (
+                        <Image id={imageId}
+                            key={hashCode(imageId.toString())}
+                            delay={index * 0.2}
+                            deleteItem={deleteItem}
+                        />
+                    )
+                }
+                )
+            }
         </div>
     )
 }
--- src/features/Applicaiton/Video.tsx
+++ src/features/Applicaiton/Video.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import { getLocalImage144p } from '../../app/handlingDatabase'
+import { deleteVideoFromDatabase, getLocalImage144p } from '../../app/handlingDatabase'
 import { motion } from 'framer-motion'
 import { useAppDispatch } from '../../app/hooks'
 import { changeTheme } from '../theme/themeSlice'
 import { changeBackground } from '../background/backgroundSlice'
 import { store } from '../../app/store'
+import * as ContextMenu from '@radix-ui/react-context-menu';
+
 interface Props {
     id: IDBValidKey,
+    delay: number,
+    deleteItem: (id: IDBValidKey) => void
 }
-const Video: React.FC<Props> = ({ id }) => {
+const Video: React.FC<Props> = ({ id, delay, deleteItem }) => {
     const [data, setData] = React.useState<string>('')
     const currentState = store.getState().theme
     React.useEffect(() => {
@@ -25,51 +29,113 @@ const Video: React.FC<Props> = ({ id }) => {
         , [])
     const dispatch = useAppDispatch();
     return (
-        <motion.div
-            initial={{
-                scale: 0,
-            }}
-            animate={{
-                scale: 1,
-                transition: {
-                    delay: 0.2,
-                    duration: 0.2
-                }
-            }}
-            onClick={() => {
-                dispatch(changeTheme({
-                    ...currentState,
-                    backgroundType: 'video',
-                    backgroundValue: String(id)
-                }))
-                dispatch(changeBackground({
-                    type: 'video',
-                    value: String(id)
-                }))
-            }}
-            whileHover={{
-                scale: 21 / 20,
-                x: "0.5rem",
-                y: (21 / 20) / 2 * 0.5 + "rem",
-            }}
-            transition={{
-                duration: 0.5,
-                ease: 'easeInOut',
-            }}
-            style={{
-                backgroundImage: `url(${data})`,
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center',
-                height: "14rem",
-                width: '20rem',
-                display: 'inline-block',
-                flex: "0 0 auto",
-                margin: '0 0.5rem',
-            }}
-            className='w-full h-full'
-        >
-        </motion.div>
+        <ContextMenu.Root>
+            <ContextMenu.Trigger className="ContextMenuTrigger">
+
+                <motion.div
+                    initial={{
+                        scale: 0,
+                    }}
+                    animate={{
+                        scale: 1,
+                        transition: {
+                            delay,
+                            duration: 0.2
+                        }
+                    }}
+                    onClick={() => {
+                        dispatch(changeTheme({
+                            ...currentState,
+                            backgroundType: 'video',
+                            backgroundValue: String(id)
+                        }))
+                        dispatch(changeBackground({
+                            type: 'video',
+                            value: String(id)
+                        }))
+                    }}
+                    whileHover={{
+                        scale: 21 / 20,
+                        x: "0.5rem",
+                        y: (21 / 20) / 2 * 0.5 + "rem",
+                    }}
+                    transition={{
+                        duration: 0.5,
+                        ease: 'easeInOut',
+                    }}
+                    style={{
+                        backgroundImage: `url(${data})`,
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundPosition: 'center',
+                        height: "14rem",
+                        width: '20rem',
+                        display: 'inline-block',
+                        flex: "0 0 auto",
+                        margin: '0 0.5rem',
+                    }}
+                    className='w-full h-full'
+                >
+                </motion.div>
+            </ContextMenu.Trigger>
+            <ContextMenu.Portal>
+                <ContextMenu.Content className='text-white bg-gray-800 rounded-md p-2 
+                '
+                >
+                    <ContextMenu.Item className="hover:bg-[#7C81E3] rounded-md px-1 py-1
+                    ">
+                        View
+                    </ContextMenu.Item>
+
+
+                    <ContextMenu.Item className={`${currentState.backgroundValue !== String(id) ?
+                        "hover:bg-[#7C81E3]"
+                        : "hover:cursor-not-allowed"} 
+                    rounded-md px-1 py-1
+                    `
+                    }
+
+                        onClick={() => {
+                            if (currentState.backgroundValue === String(id)) {
+                                return
+                            }
+                            dispatch(changeTheme({
+                                ...currentState,
+                                backgroundType: 'video',
+                                backgroundValue: String(id)
+                            }))
+                            dispatch(changeBackground({
+                                type: 'video',
+                                value: String(id)
+                            }))
+                        }}
+                    >
+                        Set as Background
+                    </ContextMenu.Item>
+
+
+                    <ContextMenu.Item className={`${currentState.backgroundValue !== String(id) ?
+                        "hover:bg-[#7C81E3]"
+                        : "hover:cursor-not-allowed"} 
+                    rounded-md px-1 py-1
+                    `
+                    }
+
+                        onClick={() => {
+                            if (currentState.backgroundValue === String(id)) {
+                                return
+                            }
+                            deleteVideoFromDatabase(String(id))
+                            deleteItem(id)
+                        }}
+                    >
+                        Delete
+                    </ContextMenu.Item>
+
+                </ContextMenu.Content>
+            </ContextMenu.Portal>
+        </ContextMenu.Root>
+
 
     )
 }
